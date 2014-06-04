bb6f221dbe469b4af83e3ce450de25ecc38c3270
BorislavIvanov <borislav.a.ivanov@gmail.com>
2014-06-04 22:43:20 +0300

    Added CasperTheGhost Start Project

---
+++ JS2TeamWork_TeamCasper/CasperTheGhost/Scripts/Engine.js
@@ -0,0 +1,21 @@
+﻿//TEST
+var drawField = new Kinetic.Stage({
+    container: 'canvas-container',
+    width: 800,
+    height: 600
+});
+
+var layerOne = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+    fill: 'yellowgreen',
+    stroke: '#CCCCCC',
+    x: 250,
+    y: 350,
+    width: 57,
+    height: 93
+});
+
+layer.add(rect);
+drawField.add(layerOne);
+//TEST
