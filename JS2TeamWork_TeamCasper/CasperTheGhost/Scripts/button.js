446a6430aa1ae3ab7b2728c7ec7c978b36e91f80
Dr4g0 <d_d0003@yahoo.com>
2014-06-05 10:21:03 +0300

    Add button.js, button.png, spring.js, spring.png. Test in html file.

---
+++ JS2TeamWork_TeamCasper/CasperTheGhost/Scripts/button.js
@@ -0,0 +1,17 @@
+﻿function Button(x, y) {
+    var button = {
+        posX: x,
+        posY: y,
+        image: function () {
+            var imageObj = new Image();
+            imageObj.src = '../resources/button.png';
+            var innerImage = new Kinetic.Image({
+                x: this.posX,
+                y: this.posY,
+                image: imageObj,
+            });
+            return innerImage;
+        }
+    }
+    return button;
+}
---
+++ JS2TeamWork_TeamCasper/CasperTheGhost/Scripts/spring.js
@@ -0,0 +1,50 @@
+﻿function Spring(x, y, button) {
+    var spring = {
+        posX: x,
+        posY: y,
+        image: function () {
+            var imageObj = new Image();
+            imageObj.src = '../resources/spring.png';
+
+            var innerSpring = new Kinetic.Sprite({
+                x: this.posX,
+                y: this.posY,
+                image: imageObj,
+                animation: 'idle',
+                animations: {
+                    idle: [
+                      // x, y, width, height (4 frames)
+                      0, 0, 101, 122,
+                    ],
+                    fit: [
+                      // x, y, width, height (3 frames)
+                      //0, 2, 100, 60,
+                      103, 0, 101, 122,
+                      206, 0, 101, 122,
+                      310, 0, 101, 122,
+                    ]
+                },
+                frameRate: 20,
+                frameIndex: 0
+            });
+
+            // start sprite animation
+            innerSpring.start();
+
+            var frameCount = 0;
+
+            innerSpring.on('frameIndexChange', function (evt) {
+                if (innerSpring.animation() === 'fit' && ++frameCount > 3) {
+                    innerSpring.animation('idle');
+                    frameCount = 0;
+                }
+            });
+
+            button.addEventListener('click', function () {
+                innerSpring.animation('fit');
+            }, false);
+            return innerSpring;
+        }
+    }
+    return spring;
+}
