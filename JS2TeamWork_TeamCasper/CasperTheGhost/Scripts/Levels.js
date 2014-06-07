6eca60f66a8c93115be82426138c9d41555fac9e
micro3x <micro3x@abv.bg>
2014-06-07 22:28:27 +0300

    Minor Changes

--- JS2TeamWork_TeamCasper/CasperTheGhost/Scripts/Levels.js
+++ JS2TeamWork_TeamCasper/CasperTheGhost/Scripts/Levels.js
@@ -4,15 +4,16 @@
         scriptsToLoad: ['CasperObject.js', 'button.js', 'spring.js'],
         collisionObjects: [
             {
-                type : 'spring',
-                name: 'spring1',
+                name: 'spring',
+                id: 'spring1',
                 x: 417,
                 y: 280,
                 button:
                     { name: 'button', x: 550, y: 450 }
             },
-            { type: 'rect', x: 500, y: 280, width: 20, height: 210 },
-            { type: 'rect', x: 0, y: 416, width: 410, height: 20 }
+            { name: 'rect', x: 500, y: 280, width: 20, height: 210 },
+            { name: 'rect', x: 0, y: 416, width: 410, height: 20 },
+            { name: 'rect', x: 500, y: 280, width: 400, height: 20 }
         ],
     }
 ]
